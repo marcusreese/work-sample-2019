d86a0ed395a73edfdcb5cd3e6b66599cc1a23ad3
Marcus Reese <marcusoccasional@gmail.com>
2019-03-30 02:05:26 -0600

    add helpful 404 messages

--- api/src/app.spec.ts
+++ api/src/app.spec.ts
@@ -1,14 +1,20 @@
-import { app } from './app';
+import { app, helpful404 } from './app';
 import request from 'supertest';
 
-describe('GET / - a simple api endpoint', () => {
-	it('Hello API Request', async () => {
+describe('The app', () => {
+	it('returns 404 for non-existent routes part 1', async () => {
 		const result = await request(app).get('/');
-		expect(result.text).toEqual('Hey');
-		expect(result.status).toEqual(200);
+		expect(result.body).toEqual(helpful404);
+		expect(result.status).toEqual(404);
 	});
 
-	it('Does not cause 404 error for favicon', async () => {
+	it('returns 404 for non-existent routes part 2', async () => {
+		const result = await request(app).get('/api');
+		expect(result.body).toEqual(helpful404);
+		expect(result.status).toEqual(404);
+	});
+
+	it('does not cause 404 error for favicon', async () => {
 		const result = await request(app).get('/favicon.ico');
 		expect(result.status).toEqual(204);
 	});
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import { purchasesSegment, purchasesRouter } from './purchases/purchases';
 
-export const prefix = '/api/v1';
-
 export const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.use(prefix + purchasesSegment, purchasesRouter)
-
-app.get('/', (req, res) => {
-  res.send('Hey');
-});
+export const prefix = '/api/v1';
+app.use(prefix + purchasesSegment, purchasesRouter);
 
 app.get('/favicon.ico', (req, res) => res.sendStatus(204)); // No Content
+
+export const helpful404 = { errorMessage: '404 Not Found -- try /api/v1/purchases' };
+app.use(function (req, res, next) {
+	res.status(404).json(helpful404);
+});
